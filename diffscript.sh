75c3371bf59872524c0deaabec77b7675d430fb4
ofrimoran <ofrimor090@gmail.com>
2024-01-29 09:36:12 +0200

    cleaning up

--- diffscript.sh
+++ diffscript.sh
@@ -2,11 +2,11 @@
 
 mkdir -p files-directory
 
-files=$(find . -type f -name '_ti_su_p.png')
+files=$(find . -type f -name '2_1_2.gif')
 
 counter=1
 for file in $files; do
-    cp "$file" "files-directory/$counter.png"
+    cp "$file" "files-directory/$counter.gif"
     ((counter++))
 done
 
